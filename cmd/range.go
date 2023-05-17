ab47b423d0df030af9961e6bf99f5af478f95e52
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2023-05-17 16:02:41 +0200

    refactorings

--- cmd/range.go
+++ cmd/range.go
@@ -59,6 +59,7 @@ func createRangeRequest(args []string) *proto.RangeRequest {
 	if len(args) == 2 {
 		key := args[1]
 		if strings.HasSuffix(key, "*") {
+			// prefix search
 			key = strings.TrimSuffix(key, "*")
 			return &proto.RangeRequest{
 				Table:    []byte(args[0]),
@@ -66,12 +67,14 @@ func createRangeRequest(args []string) *proto.RangeRequest {
 				RangeEnd: []byte(findNextString(key)),
 			}
 		} else {
+			// get by ID
 			return &proto.RangeRequest{
 				Table: []byte(args[0]),
 				Key:   []byte(args[1]),
 			}
 		}
 	}
+	// get all
 	return &proto.RangeRequest{
 		Table:    []byte(args[0]),
 		Key:      []byte{0},
@@ -84,24 +87,6 @@ func getValue(data []byte) string {
 		return base64.StdEncoding.EncodeToString(data)
 	}
 	return string(data)
-}
-
-func findNextString(str string) string {
-	// Convert string to byte slice for mutation
-	bytes := []byte(str)
-
-	// Start from the last character and increment its byte value
-	i := len(bytes) - 1
-	for i >= 0 {
-		if bytes[i] < 255 {
-			bytes[i]++
-			break
-		}
-		bytes[i] = 0
-		i--
-	}
-
-	return string(bytes)
 }
 
 func init() {
