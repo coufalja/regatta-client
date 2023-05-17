926cc0394f20e75209ca36c63627963d0122b8d1
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2023-05-17 16:43:57 +0200

    docs

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,7 @@ var binaryData bool
 func init() {
 	RootCmd.PersistentFlags().StringVar(&endpointOption, "endpoint", "localhost:8443", "regatta API endpoint")
 	RootCmd.PersistentFlags().BoolVar(&insecureOption, "insecure", false, "allow insecure connection")
-	RootCmd.PersistentFlags().BoolVar(&binaryData, "binary", false, "avoid decoding keys and values into UTF-8 strings, but rather encode them as BASE64 strings")
+	RootCmd.PersistentFlags().BoolVar(&binaryData, "binary", false, "avoid decoding keys and values into UTF-8 strings, but rather encode them as Base64 strings")
 }
 
 func Execute() {
