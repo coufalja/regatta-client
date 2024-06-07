31e17135282e5fa913009bfa2e1b6e62689f7918
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2024-06-07 23:40:48 +0200

    enable queryng for range

--- cmd/range.go
+++ cmd/range.go
@@ -23,30 +23,34 @@ var (
 func init() {
 	RangeCmd.Flags().BoolVar(&rangeBinary, "binary", false, "avoid decoding keys and values into UTF-8 strings, but rather encode them as Base64 strings")
 	RangeCmd.Flags().Int64Var(&rangeLimit, "limit", 0, "limit number of returned items. Zero is no limit")
-	RangeCmd.Flags().Var(&rangeOutput, "output", "configure output format. Currently plain and json is supported")
+	RangeCmd.Flags().Var(&rangeOutput, "output", "configure output format. Currently plain, json and jsonl is supported")
 	RangeCmd.RegisterFlagCompletionFunc("output", outputFormatCompletion)
 	RangeCmd.Flags().BoolVar(&rangeValuesOnly, "values-only", false, "return only values")
 }
 
 // RangeCmd is a subcommand used for retrieving records from a table.
 var RangeCmd = cobra.Command{
-	Use:   "range <table> [key]",
+	Use:   "range <table> [key/prefix*] [range_end]",
 	Short: "Retrieve data from Regatta store",
 	Long: "Retrieves data from Regatta store using Range query as defined in API (https://engineering.jamf.com/regatta/api/#range).\n" +
 		"You can either retrieve all items from the Regatta by providing no key.\n" +
 		"Or you can query for a single item in Regatta by providing item's key.\n" +
 		"Or you can query for all items with given prefix, by providing the given prefix and adding the asterisk (*) to the prefix.\n" +
+		"Or you can query for all items in given range, by providing lexicographic range from start (inclusive) to end (exclusive) key\n" +
 		"When key or prefix is provided, it needs to be valid UTF-8 string.",
 	Example: "regatta-client range table\n" +
 		"regatta-client range table key\n" +
-		"regatta-client range table 'prefix*'",
-	Args: cobra.MatchAll(cobra.MinimumNArgs(1), cobra.MaximumNArgs(2)),
+		"regatta-client range table 'prefix*'\n" +
+		"regatta-client range table key range_end",
+	Args: cobra.MatchAll(cobra.MinimumNArgs(1), cobra.MaximumNArgs(3)),
 	ValidArgsFunction: func(_ *cobra.Command, args []string, _ string) ([]string, cobra.ShellCompDirective) {
 		var comps []string
 		if len(args) == 0 {
 			comps = cobra.AppendActiveHelp(comps, "You must specify a Regatta table name")
 		} else if len(args) == 1 {
 			comps = cobra.AppendActiveHelp(comps, "You can provide a key or prefix to search for. If not provided all items from the table is returned.")
+		} else if len(args) == 2 {
+			comps = cobra.AppendActiveHelp(comps, "You can provide range_end, which has to be lexicographically greater than provided key")
 		}
 		return comps, cobra.ShellCompDirectiveNoFileComp
 	},
@@ -139,6 +143,11 @@ func jsonLinePrint(cmd *cobra.Command, resp *client.GetResponse) {
 }
 
 func keyAndOptsForRange(args []string) (string, []client.OpOption) {
+	if len(args) == 3 {
+		start := args[1]
+		end := args[2]
+		return start, []client.OpOption{client.WithRange(end), client.WithLimit(rangeLimit)}
+	}
 	if len(args) == 2 {
 		key := args[1]
 		if strings.HasSuffix(key, "*") {
